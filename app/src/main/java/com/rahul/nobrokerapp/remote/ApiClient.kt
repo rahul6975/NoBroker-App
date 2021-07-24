92798a01c369427daf2e357101558e9d40833d61
rahul6975 <rahulya7569@gmail.com>
2021-07-24 12:22:13 +0530

    network api added

---
+++ app/src/main/java/com/rahul/nobrokerapp/remote/ApiClient.kt
@@ -0,0 +1,13 @@
+package com.rahul.nobrokerapp.remote
+
+import com.rahul.nobrokerapp.modelClass.ResponseClass
+import retrofit2.Call
+import retrofit2.http.GET
+
+//this interface specifies the request type as GET
+
+interface ApiClient {
+
+    @GET("b/60fa8fefa917050205ce5470")
+    fun getResponse(): Call<ResponseClass>
+}
