bc313506f052e1df07b41a7083875c6ee6f30eb7
rahul6975 <rahulya7569@gmail.com>
2021-07-24 16:21:38 +0530

    api saved to database

--- app/src/main/java/com/rahul/nobrokerapp/viewHolder/ListHolder.kt
+++ app/src/main/java/com/rahul/nobrokerapp/viewHolder/ListHolder.kt
@@ -6,6 +6,7 @@ import com.bumptech.glide.Glide
 import com.rahul.nobrokerapp.interfaces.ClickListener
 import com.rahul.nobrokerapp.room.ListEntity
 import kotlinx.android.synthetic.main.item_layout.view.*
+import java.lang.StringBuilder
 
 class ListHolder(private val view: View, private val clickListener: ClickListener) :
     RecyclerView.ViewHolder(view) {
@@ -14,7 +15,12 @@ class ListHolder(private val view: View, private val clickListener: ClickListene
         view.apply {
             Glide.with(itemImage).load(listEntity.url).into(itemImage)
             tvTitle.text = listEntity.title
-            tvSubTitle.text = listEntity.subtitle
+            var stringBuilder = StringBuilder()
+            var stringArray = listEntity.subtitle.split(" ")
+            for (i in 0..stringArray.size / 4) {
+                stringBuilder.append(stringArray[i]+" ")
+            }
+            tvSubTitle.text = stringBuilder.toString()
         }
         view.setOnClickListener {
             clickListener.onClick(adapterPosition)
